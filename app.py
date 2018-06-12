069177545d1fa115b6be717b2385b9a11f068676
chung3011 <chung.hitc97@gmail.com>
2018-06-12 21:57:34 +0700

    scroll page

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ def index():
             session['income']= income
             session['goal']=goal
             session['saving']=saving
+            session['bank']=bank
+            session['month']=month
+
             if saving > float(max) :
                 error = 2
                 return render_template('error.html',error=error)
@@ -37,9 +40,22 @@ def saving():
     labels = ["Thu nhập","Tiết kiệm"]
     pie_labels = labels
     pie_values = [session['income'], session['saving']]
-    bar_labels = labels
-    bar_values=[session['income'], session['saving']]
-    return render_template('saving.html', max=200, set=zip(pie_values, labels, colors), labels=bar_labels, values=bar_values)
+
+    bar_values= []
+    bar_labels = []
+
+    month = int(session['month'])
+    bank = float(session['bank'])
+    saving = float(session['saving'])
+    var = 0
+    for i in range(int(month)):
+        name = "Tháng thứ " + str(i + 1)
+        var = round(var + saving*(pow((1+bank/12),i)),3)
+        bar_labels.append(name)
+        bar_values.append(var)
+    print(bar_values)
+
+    return render_template('saving.html', max=200, set=zip(pie_values, labels, colors),labels=bar_labels, values=bar_values)
 
 if __name__ == '__main__':
   app.run(debug=True)
