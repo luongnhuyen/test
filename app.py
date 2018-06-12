0b1bda95d3616f844a8988a22da11c31d8247d8e
chung3011 <chung.hitc97@gmail.com>
2018-06-12 16:53:30 +0700

    update error

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.secret_key = "123456789"
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
+    error = 0
     if request.method == "GET":
         return render_template('index.html')
     elif request.method == "POST":
@@ -17,21 +18,23 @@ def index():
         max = form['max']
         bank = form['bank']
         if income == "" or goal == "" or month == "" or max == "" or bank == "" :
-            return "Nhập lỗi"
+            error = 1
+            return render_template('error.html',error=error)
         else:
             saving = round(((int(goal)*float(bank)/12)/(pow((1+float(bank)/12),int(month))-1)),3)
             session['income']= income
             session['goal']=goal
             session['saving']=saving
             if saving > float(max) :
-                return "Ồ, bạn thật là tham vọng, để đạt được mục tiêu cần thêm thời gian hoặc tìm cách tăng thêm thu nhập."
+                error = 2
+                return render_template('error.html',error=error)
             else:
                 return redirect (url_for('saving'))
 
 @app.route('/saving')
 def saving():
     colors = ["#f54844", "#dbdad3"]
-    labels = ["income","saving"]
+    labels = ["Thu nhập","Tiết kiệm"]
     pie_labels = labels
     pie_values = [session['income'], session['saving']]
     bar_labels = labels
