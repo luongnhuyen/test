03ef99f5ebc160743d4241139e9ad04035ec8961
chung3011 <chung.hitc97@gmail.com>
2018-06-14 20:02:38 +0700

    update all

--- static/js/scroll.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('a[href^="#searcharea"]').on('click', function (event) {
-    event.preventDefault();
-    $("html,body").animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-  });
-});
