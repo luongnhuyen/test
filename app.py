eb9c163176e18ef940508440998678668837ce8e
oanhharry <kimoanhnt.aof@gmail.com>
2018-06-17 12:38:43 +0700

    limit số gợi ý mỗi lần

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import *
 from mongoengine import *
 import mlab
+from random import randint, choice
 from models.service import Service
 mlab.connect()
 app = Flask(__name__)
@@ -99,46 +100,49 @@ def analysis():
                 n3=round(n3*money/100,2)
                 n4=round(n4*money/100,2)
                 n5=round(n5*money/100,2)
-                food = round(n1*0.5,2)
-                go = round(n1*0.2,2)
+                food = round(n1*0.6,2)
+                go = round(n1*0.1,2)
                 life = round(n1*0.2,2)
                 other = round(n1*0.1,2)
                 book = round(n2*0.4,2)
                 training = round(n2*0.6,2)
-                all_food = Service.objects(type = "food",price__lte = food*1000000/90)
-                all_book = Service.objects(type = "book",price__lte = n2*1000000)
-                all_luxury = Service.objects(type = "luxury",price__lte = n3*1000000)
-                all_charity = Service.objects(type = "charity",price__lte = n4*1000000)
-                all_invest = Service.objects(type = "invest",price__lte = n5*1000000)
+
+
+                all_food = Service.objects(type = "food",price__lte = food*1000000/90).limit(6)
+
+                all_book = Service.objects(type = "book",price__lte = n2*1000000).limit(6)
+                all_luxury = Service.objects(type = "luxury",price__lte = n3*1000000).limit(6)
+                all_charity = Service.objects(type = "charity",price__lte = n4*1000000).limit(6)
+                all_invest = Service.objects(type = "invest",price__lte = n5*1000000).limit(6)
                 count_food = len(all_food)
                 count_book = len(all_book)
                 count_luxury = len(all_luxury)
                 count_charity = len(all_charity)
                 count_invest = len(all_invest)
                 return render_template('spending.html',
-                money = money,
-                n1=n1,
-                n2=n2,
-                n3=n3,
-                n4=n4,
-                n5=n5,
-                food = food,
-                go = go,
-                life =life,
-                other =other,
-                book = book,
-                training = training,
-                all_food = all_food,
-                all_book = all_book,
-                all_luxury = all_luxury,
-                all_charity = all_charity,
-                all_invest = all_invest,
-                count_food = count_food,
-                count_book = count_book,
-                count_luxury = count_luxury,
-                count_charity = count_charity,
-                count_invest = count_invest
-                )
+                                        money = money,
+                                        n1=n1,
+                                        n2=n2,
+                                        n3=n3,
+                                        n4=n4,
+                                        n5=n5,
+                                        food = food,
+                                        go = go,
+                                        life =life,
+                                        other =other,
+                                        book = book,
+                                        training = training,
+                                        all_food = all_food,
+                                        all_book = all_book,
+                                        all_luxury = all_luxury,
+                                        all_charity = all_charity,
+                                        all_invest = all_invest,
+                                        count_food = count_food,
+                                        count_book = count_book,
+                                        count_luxury = count_luxury,
+                                        count_charity = count_charity,
+                                        count_invest = count_invest
+                                        )
 
 @app.route('/service')
 def service():
